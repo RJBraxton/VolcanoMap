bd7edbbdaa27cfa6c9dbe8f6866ecebb326aeb73
Richard Braxton <richard@richardbraxton.org>
2014-07-11 11:48:03 -0500

    Initial commit. Everything is currently working at this moment. Yo angular, bower install, npm install, grunt and karma both tested.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name volcanoMapApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the volcanoMapApp
+ */
+angular.module('volcanoMapApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name volcanoMapApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the volcanoMapApp
+ */
+angular.module('volcanoMapApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
