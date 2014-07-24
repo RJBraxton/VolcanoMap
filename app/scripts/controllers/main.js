5ea6c7fa15b98b46cd16106fd77b548acc06c2d9
Richard Braxton <richard@richardbraxton.org>
2014-07-24 14:51:18 -0500

    Prettied up some css things. Css is generally a mess at the moment, however. Footer is proving very problematic.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -18,46 +18,6 @@
 
   $rootScope.name = 'Home';
 
- 	$scope.map = function(){
-
-     var width = 960,
-     height = 600;
-
-     $scope.projection = d3.geo.mercator()
-     .scale((width + 1) / 2 / Math.PI)
-     .translate([width / 2, height / 2])
-     .precision(0.1);
-
-
-     var path = d3.geo.path()
-     .projection( $scope.projection);
-
-     var graticule = d3.geo.graticule()
-     .step([5, 5]);
-
-     var svg = d3.select('#map').append('svg')
-     .attr('id','world')
-     .attr('viewBox', '0 0 960 500')
-     .attr('preserveAspectRatio', 'xMidyMid slice')
-     .attr('width', '100%')
-     .attr('height', '100%');
-
-     svg.append('path')
-      .datum(graticule.outline)
-      .attr('class', 'water')
-      .attr('d', path);
-
-     d3.json('./world-110m.json', function(error, world) {
-      svg.insert('path', '.graticule')
-      .datum(topojson.feature(world, world.objects.land))
-      .attr('class', 'land')
-      .attr('d', path);
-      svg.insert('path', '.graticule')
-      .datum(topojson.mesh(world, world.objects.countries, function(a, b) { return a.id !== b.id; }))
-      .attr('class', 'borders')
-      .attr('d', path);
-      }); //d3.json
-    }; //$scope.map
 
     $scope.volcanoPrep = function(){
       $scope.volcanoesReported = [];
@@ -104,6 +64,7 @@
   }; //$scope.volcanoPrep
 
   $scope.plot = function(){
+    d3.select('.cover').remove();
 
 
     var quakes = d3.select('#world').append('g')
@@ -120,6 +81,64 @@
     .on('click', function(d){ $scope.display(d,d3.select(this));});
 
   }; //$scope.plot
+
+  $scope.map = function(){
+
+     var width = 960,
+     height = 600;
+
+     $scope.projection = d3.geo.mercator()
+     .scale((width + 1) / 2 / Math.PI)
+     .translate([width / 2, height / 2])
+     .precision(0.1);
+
+
+     var path = d3.geo.path()
+     .projection( $scope.projection);
+
+     var graticule = d3.geo.graticule()
+     .step([5, 5]);
+
+     var svg = d3.select('#map').append('svg')
+     .attr('id','world')
+     .attr('viewBox', '0 0 960 500')
+     .attr('preserveAspectRatio', 'xMidyMid slice')
+     .attr('width', '100%')
+     .attr('height', '100%');
+
+     svg.append('path')
+      .datum(graticule.outline)
+      .attr('class', 'water')
+      .attr('d', path);
+
+     d3.json('./world-110m.json', function(error, world) {
+      svg.insert('path', '.graticule')
+      .datum(topojson.feature(world, world.objects.land))
+      .attr('class', 'land')
+      .attr('d', path);
+      svg.insert('path', '.graticule')
+      .datum(topojson.mesh(world, world.objects.countries, function(a, b) { return a.id !== b.id; }))
+      .attr('class', 'borders')
+      .attr('d', path);
+
+      var cover = svg.append('g').attr('class','cover');
+
+      cover.append('rect')
+      .attr('width', '100%')
+      .attr('height', '100%')
+      .style('fill', 'gray')
+      .style('opacity', 0.2)
+      .on('click', function(){ $scope.plot(); });
+
+      cover.append('text')
+      .attr('dx', '50%')
+      .attr('dy', '50%')
+      .attr('text-anchor', 'middle')
+      .text("Click to load volcano data");
+      }); //d3.json
+
+
+    }; //$scope.map
 
   $scope.display = function(d,circle){
     $scope.$apply(function(){
